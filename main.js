0e231aecb017164a4628927873105ecb74cec6bd
Achyut Padaliya <133667312+achyutpadliya@users.noreply.github.com>
2024-06-06 16:25:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+sound1 = "";
+status = "";
+objects = [];
+
+function preload(){
+   sound1 = loadSound("extreamalarm.mp3")
+}
+
+function setup(){
+    canvas=createCanvas(380,380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects"
+}
+
+function modelLoaded(){
+    console.log("Model Loaded !!")
+    status = true;
+}
+
+function gotResults(error,results){
+    if(error){
+        ocnsole.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw(){
+    image(video, 0, 0, 380,380);
+    objectDetector.detect(video, gotResults);
+    if(status !=""){
+         r = random(255)
+         g = random(255)
+         b = random(255)
+        objectDetector.detect(video, gotResults);
+        for(i = 0; i<objects.length; i++){
+            if(objects[i].label=="person"){
+                document.getElementById("status").innerHTML = "Status : Baby found";
+                sound1.stop();
+            }
+            else{
+                document.getElementById("status").innerHTML = "Status : Baby not-found";
+                sound1.play();
+            }
+            fill(r,g,b)
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " "+percent + "%", objects[i].x, objects[i].y)
+            noFill()
+            stroke(r,g,b);
+            rect(objects[i].x, objects[i].y,objects[i].width,objects[i].height);
+        }
+    }
+}
